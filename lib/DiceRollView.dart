5c7ae6fbda15891b77c3724ea71c94fa86f0d4ed
Ashu Sharma <ashusharma@Ashus-MacBook-Pro.local>
2022-01-10 13:31:37 +0530

    Changes

--- lib/DiceRollView.dart
+++ lib/DiceRollView.dart
@@ -1,5 +1,6 @@
 import 'dart:math';
 
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_cube/flutter_cube.dart';
 import 'package:flutterdicerollapp/DiceRollViewModel.dart';
@@ -12,7 +13,10 @@ class DiceRollView extends StatefulWidget {
   _DiceRollViewState createState() => _DiceRollViewState();
 }
 
-class _DiceRollViewState extends State<DiceRollView> {
+class _DiceRollViewState extends State<DiceRollView>
+    with TickerProviderStateMixin {
+  AnimationController controller;
+
   Map<int, Vector3> _diceRolls = {
     5: Vector3(0, 0, 0),
     6: Vector3(90, 0, 0),
@@ -21,8 +25,31 @@ class _DiceRollViewState extends State<DiceRollView> {
     3: Vector3(0, 90, 0),
     4: Vector3(0, 270, 0),
   };
+  Object dice;
+  Animation<Vector3> roll;
+  Vector3 lastRoll = Vector3(0, 0, 0);
 
   int n = 5;
+
+  AudioCache _audioCache;
+
+  @override
+  void initState() {
+    controller =
+        AnimationController(duration: Duration(milliseconds: 600), vsync: this);
+    dice = Object(fileName: "assets/Dice.obj", scale: Vector3.all(3.0));
+    controller.addListener(() {
+      dice.rotation.setFrom(roll.value);
+      dice.updateTransform();
+      setState(() {});
+    });
+    super.initState();
+    _audioCache = AudioCache(
+      prefix: 'assets/audio/',
+      fixedPlayer: AudioPlayer()..setReleaseMode(ReleaseMode.STOP),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider(
@@ -37,30 +64,50 @@ class _DiceRollViewState extends State<DiceRollView> {
                     height: 40.0,
                   ),
                   Spacer(),
-                  Image.asset(
-                    'images/' + "dice_${model.diceNumber}.png",
-                    height: 150,
-                    width: 150,
-                  ),
                   Container(
                     height: 500,
                     width: 500,
                     child: Cube(
                       onSceneCreated: (Scene scene) {
-                        scene.world.add(Object(
-                            fileName: 'assets/Dice.obj',
-                            name: "cube",
-                            rotation: _diceRolls[n],
-                            scale: Vector3.all(3)));
+                        scene.world.add(dice);
                       },
                     ),
                   ),
                   Spacer(),
+                  if (controller.isCompleted)
+                    Text(
+                      "You got $n",
+                      style: TextStyle(color: Colors.white, fontSize: 25.0),
+                    ),
+                  Spacer(),
                   GestureDetector(
-                    onTap: () {
-                      setState(() {
+                    onTap: () async {
+                      n = Random().nextInt(6) + 1;
+                      if (n == 6) {
                         n = Random().nextInt(6) + 1;
-                      });
+                        if (n == 6) {
+                          n = Random().nextInt(6) + 1;
+                        }
+                      } else if (n == 5 || n == 4) {
+                        n = Random().nextInt(6) + 1;
+                      }
+                      _audioCache.play('roll.mp3');
+                      print(n);
+                      var temp = Random().nextInt(360);
+                      roll = Tween<Vector3>(
+                              begin: lastRoll, end: Vector3.all(temp + 0.0))
+                          .animate(CurvedAnimation(
+                              parent: controller, curve: Curves.linear));
+                      controller.reset();
+                      await controller.forward();
+                      roll = Tween<Vector3>(
+                              begin: Vector3.all(temp + 0.0),
+                              end: _diceRolls[n])
+                          .animate(CurvedAnimation(
+                              parent: controller, curve: Curves.easeIn));
+                      lastRoll = _diceRolls[n];
+                      controller.reset();
+                      controller.forward();
                     },
                     child: Container(
                       margin: EdgeInsets.all(20.0),
